ec8cf52ac008359d265e5410819b41a46f26288d
YASUOKA Masahiko <yasuoka@yasuoka.net>
2024-12-10 15:05:20 +0900

    fix a typo

--- captcha.c
+++ captcha.c
@@ -7,7 +7,7 @@ captcha_code_is_valid(const char *code)
 	if (code == NULL)
 		return false;
 
-	/* TODO: you check code must be here */
+	/* TODO: your check code must be here */
 
 	return false;
 }
